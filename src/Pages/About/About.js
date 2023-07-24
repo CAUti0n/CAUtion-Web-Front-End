72349e8007569062ecb2ed72a227b25d56f7187f
Lim-shweety <smile70200@naver.com>
2023-07-24 16:56:47 +0900

    bar 이미지 추가 및 nav수정

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Nav from '../../Components/Nav'
+import styles from "../Home/styles/Home.module.css";
 
 const About = () => {
   return (
     <div>
+        <Nav />
+        <img src={`./images/bar.png`}></img>
 
-      <Nav />
+
 
       <h1>about</h1>
     </div>
---
+++ src/Pages/Management/Management.js
@@ -0,0 +1,16 @@
+import Nav from "../../Components/Nav";
+import React from "react";
+
+const Management = () => {
+    return (
+        <div>
+
+            <Nav />
+
+            <h1>Management</h1>
+
+        </div>
+    )
+}
+
+export default Management
