be5c60ec936db63cfb600aeaaf49ccc7894a0b67
Lim-shweety <smile70200@naver.com>
2023-08-05 18:57:29 +0900

    management페이지와 study 페이지 미완. footer제작중

---
+++ src/Components/footer/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import {SlLocationPin,SlEnvolope} from "react-icons/sl";
+
+import {SiGithub} from "react-icons/si";
+const HallWrapper=styled.div`
+    background-color: #00b992;
+`
+const TitleWrapper=styled.div`
+color:white;
+font-size:30px;
+font-family: 'Do Hyeon', sans-serif;
+margin-left:10px;
+`
+const ContentWrapper=styled.div`
+    color:white;
+    font-family: 'Do Hyeon', sans-serif;
+    font-size:20px;
+`
+const Icon=styled.div`
+width:30px;
+`
+const Footer=()=>{
+    return(
+        <HallWrapper>
+           
+            <Container>
+                <Row>
+                    <Col xs={1} md={1}></Col>
+                    <Col md={8}>
+                 <TitleWrapper>
+                Get In Touch
+                </TitleWrapper>
+                <ContentWrapper>
+                    <SlLocationPin style={{"color":"white","margin":"5px","width":"25px","height":"25px",}}/>
+                      Chung-Ang Univ.                    Computer Science & Engineering
+                    <br/>
+                    <SlEnvolope  style={{"color":"white","margin":"5px","width":"25px","height":"25px"}}/>
+                    2021caution@gmail.com
+                    <br/>
+                    <SiGithub style={{"color":"white","margin":"5px","width":"25px","height":"25px"}}/>
+                    https://github.com/CAUti0n
+                </ContentWrapper>
+                
+                </Col>
+                </Row>
+            </Container>
+        </HallWrapper>
+    );
+}
+
+
+export default Footer;
--- src/Pages/Study/Study.js
+++ src/Pages/Study/Study.js
@@ -1,18 +1,56 @@
 import React from 'react'
-
 import Nav from '../../Components/Nav'
 import Title from "../../Components/title/Title";
+import styled from "styled-components";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import StudyCard from "../../Components/card/StudyCard";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Study = () => {
-  return (
-    <div>
+const Wrapper = styled.div`
+  padding-right: 100px;
+  padding-left: 100px;
 
-      <Nav />
+`
+const Year = styled.div`
+  font-size: 35px;
+  font-weight: bold;
+  margin : 3px;
+`
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 
-        <Title props={'Study'}/>
+`
+const Study = ({props}) => {
+    return (
+        <div>
 
-    </div>
-  )
+            <Nav/>
+            <Title props={'Study'}/>
+            <Wrapper>
+                <Container>
+                    <Row>
+                        <Col xs={1} md={2}></Col>
+                        <Col xs={10} md={8}>
+                            <Year>
+                                2023-1
+                            </Year>
+                            <Cards>
+                                <StudyCard></StudyCard>
+                                <StudyCard></StudyCard>
+                                <StudyCard></StudyCard>
+                                <StudyCard></StudyCard>
+                                <StudyCard></StudyCard>
+                            </Cards>
+                        </Col>
+                        <Col md={2}></Col>
+                    </Row>
+                </Container>
+            </Wrapper>
+        </div>
+    )
 }
 
 export default Study
