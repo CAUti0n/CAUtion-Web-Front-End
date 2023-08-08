4cb904698b9a9ffaffc8121c923986b2066dc555
yeojin <yeojin7010+L@gmail.com>
2023-08-08 20:52:19 +0900

    feat: add studyAPI to server

--- src/Pages/Study/Study.js
+++ src/Pages/Study/Study.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import Nav from '../../Components/Nav'
+import React from "react";
+import Nav from "../../Components/Nav";
 import Title from "../../Components/title/Title";
 import styled from "styled-components";
 import StudyCard from "../../Components/study/StudyCard";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../../Components/footer/Footer";
+import result from "./study.json";
 
 const Wrapper = styled.div`
   position: relative;
@@ -12,58 +13,56 @@ const Wrapper = styled.div`
   display: block;
   justify-content: center;
   align-content: center;
-`
+`;
+
 const Year = styled.div`
-  display:flex;
+  display: flex;
   font-size: 35px;
   font-weight: bold;
   padding-left: 3px;
-  margin-left:auto;
+  margin-left: auto;
   margin-right: auto;
-  width:784px;
-  align-items:initial;
-  justify-content: initial;  
-  
-`
+  width: 784px;
+  align-items: initial;
+  justify-content: initial;
+`;
+
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 784px;
-  margin-left:auto;
+  margin-left: auto;
   margin-right: auto;
 
   margin-bottom: 100px;
-  
+`;
 
-`
-const Study = ({props}) => {
-    return (
-        <div>
+const Study = ({ props }) => {
+  console.log(result[0]["properties"]["Manager"]["rich_text"][0]["plain_text"]);
+  return (
+    <div>
+      <Nav />
+      <Title props={"Study"} style={{ "margin-bottom": "0px" }} />
+      <Wrapper>
+        <Year>2023-1</Year>
+        <Cards>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+        </Cards>
+        <Year>2023-1</Year>
+        <Cards>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+          <StudyCard></StudyCard>
+        </Cards>
+      </Wrapper>
+      <Footer />
+    </div>
+  );
+};
 
-            <Nav/>
-            <Title props={'Study'} style={{"margin-bottom":"0px"}}/>
-
-            <Wrapper>
-                <Year>2023-1</Year>
-                <Cards>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                </Cards>
-                <Year>2023-1</Year>
-                <Cards>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                    <StudyCard></StudyCard>
-                </Cards>
-            </Wrapper>
-            <Footer/>
-        </div>
-    )
-}
-
-export default Study
+export default Study;
