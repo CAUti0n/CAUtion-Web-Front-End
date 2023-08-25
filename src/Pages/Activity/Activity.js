5795fbb73984d25db4ded4cc49985a435b3ab529
이주형 JuhyeongLee <smilebank7@naver.com>
2023-08-25 22:52:17 +0900

    Activity & 배너 요청사항 반영 2
    1. 배너 반응형+ 간격 임의 조정
    2. activity 반응형 및 요청사항 반영( 다만 card의 정렬이 완벽하지 못함)

--- src/Pages/Activity/Activity.js
+++ src/Pages/Activity/Activity.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './styles/Activity.module.css';
 import Nav from '../../Components/Nav';
 import Title from '../../Components/title/Title';
 import Footer from '../../Components/footer/Footer';
-import ActivityPage1 from './page1'; // import each page component
+import ActivityPage1 from './page1';
 import ActivityPage2 from './page2';
 import ActivityPage3 from './page3';
-
+import styles from './styles/Activity.module.css'
 
 const Wrapper = styled.div`
-  padding-right: 100px;
-  padding-left: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   min-height: calc(80vh - 160px);
+  margin: auto;
 `;
 
 const StoneLink = styled(Link)`
@@ -37,48 +35,41 @@ const StoneImage = styled.img`
 `;
 
 const StoneItem = styled.div`
-  position: relative;
   display: inline-block;
-  margin: 0 20px;
+  margin: 0 10px;
 `;
 
 const StoneNumber = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: absolute;
   top: 58%;
   left: 50%;
   transform: translate(-50%, -50%);
-  opacity: 0.8; /*호버 전 투명도입니당*/
-  font-size: 20px;
+  font-size: 2vw;
   font-weight: bold;
   color: white;
   transition: opacity 0.3s ease-in-out;
-
-  &:hover {
-  }
+  opacity: ${({ active }) => (active ? '1' : '0.5')};
 `;
 
 const StoneImageContainer = styled.div`
   position: relative;
-  width: 130px; /* 원하는 크기로 조절 */
-  height: 130px; /* 원하는 크기로 조절 */
-  &:hover ${StoneNumber} {
-    opacity: 1;
-  }
+  width: 10vw;
+  height: 10vw;
 `;
 
 const CenteredContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100vh;
-  transition: opacity 0.5s ease-in-out; // 투명도 트랜지션
-  opacity: ${(props) => (props.visible ? 1 : 0)}; // visible 상태에 따라 투명도 설정
+  min-height: 100vh;
 `;
 
-const Activity = () => {
 
-  const [activeStone, setActiveStone] = useState(null);
+const Activity = () => {
+  const [activeStone, setActiveStone] = useState('stone3');
 
   const handleStoneClick = (stone) => {
     if (activeStone === stone) {
@@ -92,67 +83,52 @@ const Activity = () => {
       <div>
         <Nav />
         <Title props={'Activity'} />
-        <Container>
-          <Row>
-            <Col xs={1} md={2}></Col>
-            <Col md={8}>
-              <Wrapper>
-                <div className={styles.arrowContainer}>
-                  <StoneLink onClick={() => handleStoneClick('stone1')} className={styles.stoneItem}>
-                    <StoneItem>
-                      <StoneImageContainer>
-                        <StoneImage src="/img/stone-image.png" alt="Activity Page" />
-                        <StoneNumber className={styles.stoneNumber}>2021</StoneNumber>
-                      </StoneImageContainer>
-                    </StoneItem>
-                  </StoneLink>
-
-
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-
-
-                  <StoneLink onClick={() => handleStoneClick('stone2')} className={styles.stoneItem}>
-                    <StoneItem>
-                      <StoneImageContainer>
-                        <StoneImage src="/img/stone-image.png" alt="Activity Page" />
-                        <StoneNumber className={styles.stoneNumber}>2022</StoneNumber>
-                      </StoneImageContainer>
-                    </StoneItem>
-                  </StoneLink>
-
-
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-                  <div className={styles.arrow}></div>
-
-
-                  <StoneLink onClick={() => handleStoneClick('stone3')} className={styles.stoneItem}>
-                    <StoneItem>
-                      <StoneImageContainer>
-                        <StoneImage src="/img/stone-image.png" alt="Activity Page" />
-                        <StoneNumber className={styles.stoneNumber}>2023</StoneNumber>
-                      </StoneImageContainer>
-                    </StoneItem>
-                  </StoneLink>
-
-
-                </div>
-                <CenteredContainer visible={activeStone !== null}>
-                  {activeStone === 'stone1' && <ActivityPage1 />}
-                  {activeStone === 'stone2' && <ActivityPage2 />}
-                  {activeStone === 'stone3' && <ActivityPage3 />}
-                </CenteredContainer>
-              </Wrapper>
-            </Col>
-          </Row>
-        </Container>
+        <Wrapper>
+          <div className={styles.arrowContainer}>
+            <StoneLink onClick={() => handleStoneClick('stone1')} className={styles.stoneItem}>
+              <StoneItem>
+                <StoneImageContainer>
+                  <StoneImage src="/img/stone-image.png" alt="Activity Page" />
+                  <StoneNumber className={styles.stoneNumber} active={activeStone==='stone1'}>2021</StoneNumber>
+                </StoneImageContainer>
+              </StoneItem>
+            </StoneLink>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <StoneLink onClick={() => handleStoneClick('stone2')} className={styles.stoneItem}>
+              <StoneItem>
+                <StoneImageContainer>
+                  <StoneImage src="/img/stone-image.png" alt="Activity Page" />
+                  <StoneNumber className={styles.stoneNumber}  active={activeStone==='stone2'}>2022</StoneNumber>
+                </StoneImageContainer>
+              </StoneItem>
+            </StoneLink>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <div className={styles.arrow}></div>
+            <StoneLink onClick={() => handleStoneClick('stone3')} className={styles.stoneItem}>
+              <StoneItem>
+                <StoneImageContainer>
+                  <StoneImage src="/img/stone-image.png" alt="Activity Page" />
+                  <StoneNumber className={styles.stoneNumber}  active={activeStone==='stone3'}>2023</StoneNumber>
+                </StoneImageContainer>
+              </StoneItem>
+            </StoneLink>
+          </div>
+          <CenteredContainer visible={activeStone !== null}>
+              {activeStone === 'stone1' && <ActivityPage1 />}
+              {activeStone === 'stone2' && <ActivityPage2 />}
+              {activeStone === 'stone3' && <ActivityPage3 />}
+          </CenteredContainer>
+        </Wrapper>
         <Footer />
       </div>
   );
 };
 
 export default Activity;
+
+
